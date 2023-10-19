91493ab39b89cadfebea406cbe380f78d93412a6
Guillermo García <guijujo.25@gmail.com>
2023-10-19 17:16:01 -0300

    Se agregó la opción de eliminar un libro

--- src/controller.js
+++ src/controller.js
@@ -7,8 +7,8 @@ class LibrosController {
     }
 
     async getOne(req,res){
-        const id = req.body.id;
-        const [result] = await pool.query('SELECT * FROM libros WHERE id = ?', [id]);
+        const libro = req.body;
+        const [result] = await pool.query('SELECT * FROM libros WHERE id = ?', [libro.id]);
         if (result.length > 0) {
           // Devolver el libro encontrado
           res.json(result[0]);
@@ -22,6 +22,16 @@ class LibrosController {
       const libro = req.body;
       const [result] = await pool.query(`INSERT INTO Libros(nombre, autor, categoria, año, ISBN) VALUES (?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.año, libro.ISBN]);
       res.json({"ID insertado": result.insertId, "message": "Libro insertado exitosamente"});
+    }
+
+    async delete(req,res){
+      const libro = req.body;
+      const [result] = await pool.query(`DELETE FROM Libros WHERE ISBN = (?)`, [libro.ISBN]);
+      if (result.affectedRows > 0) {
+        res.json({ "message": `Libro con ISBN ${libro.ISBN} eliminado exitosamente` });
+      } else {
+        res.status(404).json({ "Error": `No se encontró ningún libro con el ISBN ${libro.ISBN}` });
+      }
     }
 }
 
