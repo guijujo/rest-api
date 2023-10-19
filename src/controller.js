70c955b80bad2a4a8ae982badcae5617bd15d077
Guillermo García <guijujo.25@gmail.com>
2023-10-19 11:14:30 -0300

    Alta de libros

--- src/controller.js
+++ src/controller.js
@@ -17,6 +17,12 @@ class LibrosController {
           res.status(404).json({"Error": `No se encontró el libro con el id ${id}`});
         }
     }
+
+    async add(req,res){
+      const libro = req.body;
+      const [result] = await pool.query(`INSERT INTO Libros(nombre, autor, categoria, año, ISBN) VALUES (?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.año, libro.ISBN]);
+      res.json({"ID insertado": result.insertId, "message": "Libro insertado exitosamente"});
+    }
 }
 
 export const libro = new LibrosController();
