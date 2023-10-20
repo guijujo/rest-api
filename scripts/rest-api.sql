1be9b4053cc82a1b082c67da60214091b282e00c
Guillermo García <guijujo.25@gmail.com>
2023-10-20 17:45:49 -0300

    Se agregó nueva base de datos

--- scripts/rest-api.sql
+++ scripts/rest-api.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-10-2023 a las 23:03:59
+-- Tiempo de generación: 20-10-2023 a las 22:44:42
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 7.4.33
 
@@ -41,8 +41,16 @@ CREATE TABLE `libros` (
 --
 
 INSERT INTO `libros` (`id`, `nombre`, `autor`, `categoria`, `año`, `ISBN`) VALUES
-(1, 'Señor de los Anillos', 'J. R. R. Tolkien', 'Novela', '1954-01-01', '10 8845292614'),
-(2, 'Blancanieves', 'Hermanos Grimm', 'Niños y Jóvenes', '1937-12-21', '100374370990');
+(1, 'Señor de los Anillos', 'J. R. R. Tolkien', 'Novela', '1954-01-01', '108845292614'),
+(2, 'Blancanieves', 'Hermanos Grimm', 'Niños y Jóvenes', '1937-12-21', '100374370990'),
+(3, 'El Principito', 'Antoine de Saint-Exupéry', 'Fábula', '0000-00-00', '100156014041'),
+(6, 'Cien años de soledad', 'Gabriel García Márquez', 'Novela', '0000-00-00', '9788437604947'),
+(7, 'El código Da Vinci', 'Dan Brown', 'Thriller', '0000-00-00', '978-84-9759-9'),
+(8, 'Harry Potter y la piedra filos', 'J.K. Rowling', 'Fantasía', '0000-00-00', '978-84-7888-4'),
+(9, 'El alquimista', 'Paulo Coelho', 'Aventura', '0000-00-00', '978-84-08-024'),
+(10, 'El diario de Ana Frank', 'Ana Frank', 'Memorias', '0000-00-00', '978-84-663-11'),
+(11, 'El señor de los anillos: La co', 'J.R.R. Tolkien', 'Fantasía épica', '0000-00-00', '978-84-450-70'),
+(14, 'Don Quijote de la Mancha', 'Miguel de Cervantes', 'Novela', '0000-00-00', '978-84-376-03');
 
 --
 -- Índices para tablas volcadas
@@ -62,7 +70,7 @@ ALTER TABLE `libros`
 -- AUTO_INCREMENT de la tabla `libros`
 --
 ALTER TABLE `libros`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
